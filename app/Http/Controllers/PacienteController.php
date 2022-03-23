716e31ff9bf02315efb50d95a66a11a0947190bd
aldo.ibarra <aldo.ibarra@skytel.com.py>
2022-03-23 15:24:03 -0300

    SYMD-YYYY Test

--- app/Http/Controllers/PacienteController.php
+++ app/Http/Controllers/PacienteController.php
@@ -4,11 +4,13 @@ namespace App\Http\Controllers;
 
 use App\Models\Rossi\Paciente;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Artisan;
 
 class PacienteController extends Controller
 {
     public function getPacienteByDNI(Request $request, $dni)
     {
+        Artisan::call('rossi:insert-mas');
         return Paciente::where(Paciente::COLUMNA_NRO_DOCUMENTO, $dni)->firstOrFail();
     }
 }
