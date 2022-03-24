e50bbff3db9a67a9985684a4cf083fa92a892286
javierovico <javierovico@gmail.com>
2022-03-24 11:11:59 -0300

    SYMD-XXXX get paciente from dni valida que sea dni y retorna nuevo estandar

--- app/Http/Controllers/PacienteController.php
+++ app/Http/Controllers/PacienteController.php
@@ -11,9 +11,26 @@ class PacienteController extends Controller
 {
     public function getPacienteByDNI(Request $request, $dni)
     {
-        return Paciente::query()
+        /** @var Paciente $paciente */
+        $paciente =  Paciente::query()
             ->where(Paciente::COLUMNA_NRO_DOCUMENTO, $dni)
             ->whereHas(Paciente::RELACION_TIPO_DOCUMENTO,fn(Builder $q) => $q->where(TipoDocumento::COLUMNA_DESCRIPCION, TipoDocumento::DESCRIPCION_DNI))
-            ->firstOrFail();
+            ->firstOrFail()
+        ;
+        return [
+            'id' => $paciente->pac_id,
+            'nombre' => $paciente->pac_nombre,
+            'apellido' => $paciente->pac_apellido,
+            'tipoDocumento' => $paciente->tipoDocumento->tid_descripcion,
+            'nroDocumento' => $paciente->pac_nro_documento,
+            'calle' => $paciente->pac_domicilio_calle?:'',
+            'nroCasa' => $paciente->pac_domicilio_nro?:'',
+            'localidad' => $paciente->localidad?$paciente->localidad->loc_nombre:'',
+            'provincia' => $paciente->provincia?$paciente->provincia->pro_nombre:'',
+            'telefono' => $paciente->pac_telefono_movil,
+            'email' => $paciente->pac_email?:'',
+            'fechaNacimiento' => $paciente->pac_fecha_nacimiento,
+            'crudo' => $paciente,
+        ];
     }
 }
